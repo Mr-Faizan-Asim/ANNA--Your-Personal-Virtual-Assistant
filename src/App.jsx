b1da4b28eda9e8e2d7d9f3f48758696f23dbed03
Mr-Faizan-Asim <mr.faizan.asim@gmail.com>
2024-11-19 00:06:47 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import BotComponent from './Components/BotComponent/BotComponent'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BotComponent from "./Components/BotComponent/BotComponent";
+import EmailManager from "./Components/EmailManager/EmailManager";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <BotComponent/>
-    </div>  
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<BotComponent />} />
+          <Route path="/emails" element={<EmailManager />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
