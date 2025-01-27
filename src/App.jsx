c4fee2b2ffe8c623b2e2a9ee0d681e87494ce087
Muhammad Faizan Asim <mr.faizan.asim@gmail.com>
2025-01-28 03:12:59 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Home from './Components/Home/Home';
 
-const CLIENT_ID = "1003847576572-81o93k5rndnlq8ljcr56vqhv8k639c9h.apps.googleusercontent.com";
+const CLIENT_ID = "";
 
 function App() {
   return (
