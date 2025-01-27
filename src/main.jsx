a9232afcef776c23dcb635961e7dd58c79227598
Muhammad Faizan Asim <mr.faizan.asim@gmail.com>
2025-01-28 03:10:24 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { createClient } from '@supabase/supabase-js';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
 const supabase = createClient(
-  "https://ghvppxlkgmgbxvcecpvy.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdodnBweGxrZ21nYnh2Y2VjcHZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwOTY5ODEsImV4cCI6MjA0NzY3Mjk4MX0.YFyZasBMI60y_ol1uuofTESlibxUsfXCxV_U3SWVwsc"
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
