4efd80ab78b06e86521f4ffa1acd095eb9742416
Muhammad Faizan Asim <mr.faizan.asim@gmail.com>
2025-01-28 03:13:20 +0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,1 @@
-import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://ghvppxlkgmgbxvcecpvy.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdodnBweGxrZ21nYnh2Y2VjcHZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwOTY5ODEsImV4cCI6MjA0NzY3Mjk4MX0.YFyZasBMI60y_ol1uuofTESlibxUsfXCxV_U3SWVwsc";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
