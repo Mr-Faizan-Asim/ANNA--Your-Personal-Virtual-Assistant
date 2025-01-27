90b3088a3ebdef2c3d82a55f6bc593ce40e0dc46
Muhammad Faizan Asim <mr.faizan.asim@gmail.com>
2025-01-28 03:05:54 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import GoogleCalendarIntegration from './Components/GoogleCalendarIntegration/GoogleCalendarIntegration'; // Import your Google integration component
 
-const CLIENT_ID = "1003847576572-81o93k5rndnlq8ljcr56vqhv8k639c9h.apps.googleusercontent.com";
+const CLIENT_ID = "";
 
 function App() {
   return (
